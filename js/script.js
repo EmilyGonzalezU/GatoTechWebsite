e9a1c30e4e98ca142b7e839aba2f65414d5a1107
EmilyGonzalez <emilyy.ug27@gmail.com>
2024-04-29 19:33:20 -0400

    "Funciones"

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
-/*Auto show  */
+//Modal inicio sesion
+document.getElementById('openModalBtn').addEventListener('click', function() {
+    $('#loginModal').modal('show');
+});
+
+// Owl carousel resposivo
+$(document).ready(function() { 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            576: {
+                items: 2,
+                nav: false
+            },
+            768: {
+                items: 3,
+                nav: false
+            },
+            992: {
+                items: 4,
+                nav: false
+            },
+            1200: {
+                items: 5,
+                nav: false,
+                loop: false
+            }
+        }
+    })
+});
+
+// Auto show filtros
 
 $(document).ready(function(){
-        // Abre el primer acordeón al cargar la página
+        // Abre el primer acordeon al cargar la pagina
     $('#collapseOne').collapse('show');
 });
 
-/*DSCTO disable show */
+// Activacion/Desactivacion DSCTO
 var containerDsctoTarget = document.querySelectorAll('.dsctoTarget');
 
 containerDsctoTarget.forEach(function(dsctoTarget){
@@ -23,7 +60,7 @@ containerDsctoTarget.forEach(function(dsctoTarget){
     }
 });
 
-/**TRUNC TEXT FUNCTION */
+// Trunc text
 document.addEventListener("DOMContentLoaded", function() { /*DOM CARGADO EJECUTA FUNCION*/
     var productDescriptions = document.querySelectorAll('.productDesc');
 
@@ -35,23 +72,123 @@ document.addEventListener("DOMContentLoaded", function() { /*DOM CARGADO EJECUTA
 });
 
 
-//Validation
-  
-        (() => {
-            'use strict'
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.querySelectorAll('.needs-validation')
+// Validacion Registro usuario
+const btnEnviar = document.getElementById('btnEnviar');
 
-            // Loop over them and prevent submission
-            Array.from(forms).forEach(form => {
-                form.addEventListener('submit', event => {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+btnEnviar.addEventListener('click', (e) => {
 
-                form.classList.add('was-validated')
-                }, false)
-            })
-        })()
+    const esValido = validacion();
+    
+    
+    if (!esValido) {
+        e.preventDefault(); // Detieneee el envío del formulario si hay errores de validación
+    }
+});
+
+const validacion = () => {
+    const nombreUsuario = document.getElementById('nombre');
+    const apellidoUsuario = document.getElementById('apellido');
+    const telUsuario = document.getElementById('telefono');
+    const rutUsuario = document.getElementById('RUT');
+    const passUsuario = document.getElementById('pass');
+    const emailUsuario = document.getElementById('email');
+
+    let hayErrores = false;
+
+    //Validar nombre
+    if (nombreUsuario.value === "") {
+        document.getElementById('msgErrorNom').textContent = "Por favor, escribe tu nombre.";
+        hayErrores = true;
+    } else {
+        document.getElementById('msgErrorNom').textContent = "";
+    }
+
+    // Validar apellido
+    if (apellidoUsuario.value === "") {
+        document.getElementById('msgErrorAp').textContent = "Por favor, escribe tu apellido.";
+        hayErrores = true;
+    } else {
+        document.getElementById('msgErrorAp').textContent = "";
+    }
+
+    // Validar email
+    const emailValue = emailUsuario.value;
+    const mensajeErrorEmail = document.getElementById('msgErrorMail');
+    if (emailValue.value === "") {
+        mensajeErrorEmail.textContent = "Por favor, escribe tu correo electrónico.";
+        hayErrores = true;
+    } else {
+        if (!emailValido(emailValue)) {
+            mensajeErrorEmail.textContent = "Por favor, Ingrese correo valido.";
+        }else{
+            mensajeErrorEmail.textContent = "";
+        }
+        
+    }
+
+    // Validar teléfono
+    if (telUsuario.value === "") {
+        document.getElementById('msgErrorTel').textContent = "Por favor, escribe tu teléfono.";
+        hayErrores = true;
+    } else {
+        document.getElementById('msgErrorTel').textContent = "";
+    }
+
+    // Validar RUT 
+    const rutValue = rutUsuario.value.trim();
+    const mensajeErrorRut = document.getElementById('msgErrorRUT');
+
+    // Vacio
+    if (rutValue === "") {
+        mensajeErrorRut.textContent = "Por favor, ingresa un RUT.";
+        hayErrores = true;
+    } else {
+        // Validacion  RUT
+        if (!Fn.validaRut(rutValue)) {
+            mensajeErrorRut.textContent = "Por favor, ingresa un RUT válido.";
+            hayErrores = true;
+        } else {
+            mensajeErrorRut.textContent = "";
+        }
+    }
+
+    // Validar contraseña
+    if (passUsuario.value === "") {
+        document.getElementById('msgErrorPass').textContent = "Por favor, ingresa una contraseña válida.";
+        
+        hayErrores = true;
+    } else {
+        document.getElementById('msgErrorPass').textContent = "";
+    }
+    
+    // Si hay errores, no se puede enviar el formulario
+    return !hayErrores;
+    
+}
+
+// Validacion email @ 
+const emailValido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+// Validacion RUT 
+var Fn = {
+    validaRut: function (rutCompleto) {
+        rutCompleto = rutCompleto.replace("‐", "-");
+        if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(rutCompleto))
+            return false;
+        var tmp = rutCompleto.split('-');
+        var digv = tmp[1];
+        var rut = tmp[0];
+        if (digv == 'K') digv = 'k';
+
+        return (Fn.dv(rut) == digv);
+    },
+    dv: function (T) {
+        var M = 0, S = 1;
+        for (; T; T = Math.floor(T / 10))
+            S = (S + T % 10 * (9 - M++ % 6)) % 11;
+        return S ? S - 1 : 'k';
+    }
+}
 
