09db9f806a9d16d016526db5c435334872e6070c
Emily <em.gonzalezu@duocuc.cl>
2024-07-17 15:24:01 -0400

    "carrito v1"

--- carrito/urls.py
+++ carrito/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('restar/<int:id_producto>/', views.restar_producto, name="Sub"),
     path('limpiar/', views.limpiar_carrito, name="CLS"),
     path('carrito/', views.carrito, name="carrito"),
+    path('datos/', views.datos_usuario_compra, name="datos_usuario"),
+    path('direccion/', views.direccion, name="direccion"),
     
 
 ] 
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -49,7 +49,7 @@ class RegistroUsuarioForm(forms.ModelForm):
             user.save()
         return user
 
-
+#Datos que se pueden modificar
 class DatosPersonalesForm(forms.ModelForm):
     class Meta:
         model = PerfilUsuario
@@ -59,3 +59,69 @@ class DireccionForm(forms.ModelForm):
     class Meta:
         model = Direccion
         fields = ['calle', 'numero', 'depto_casa', 'nombre_receptor', 'apellido_receptor', 'rut_receptor', 'telefono_receptor', 'region', 'comuna']
+        widgets = {
+            'calle': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'id': 'calle',
+                'required': 'required'
+            }),
+            'numero': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'depto_casa': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'nombre_receptor': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'apellido_receptor': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'rut_receptor': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'telefono_receptor': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'region': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+            'comuna': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'required': 'required'
+            }),
+        }
+#Datos completos
+class DatosPersonalesForm2(forms.ModelForm):
+    class Meta:
+        model = PerfilUsuario
+        fields = ['nombre', 'apellido', 'email', 'telefono']
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'id': 'nombre',
+                'required': 'required'
+            }),
+            'apellido': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'id': 'apellido',
+                'required': 'required'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control bg-light',
+                'id': 'email',
+                'required': 'required'
+            }),
+            'telefono': forms.TextInput(attrs={
+                'class': 'form-control bg-light',
+                'id': 'telefono',
+                'required': 'required'
+            })
+        }
